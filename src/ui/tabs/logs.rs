dac8eb726624b0dfc2d9bdc364a5865ceb0c382d
alemi <me@alemi.dev>
2026-01-04 20:30:43 +0100

    feat: scrollbar in log tab

--- src/ui/tabs/logs.rs
+++ src/ui/tabs/logs.rs
@@ -1,21 +1,31 @@
 use ratatui::{
-	crossterm::event::{Event, KeyCode}, style::Stylize, text::Line, widgets::{Block, Paragraph, Widget, Wrap}
+	crossterm::event::{Event, KeyCode}, layout::Margin, style::{Style, Stylize}, symbols::scrollbar, text::Line, widgets::{Block, Paragraph, Scrollbar, ScrollbarState, StatefulWidget, Widget, Wrap}
 };
 
 use crate::ui::modifier_magnitude;
 
 pub struct LogsTab {
-	// TODO more sophisticated scrolls? maybe scrollbar?
-	scroll: u16,
+	scroll: usize,
+	scroll_state: ScrollbarState,
 }
 
 impl super::Tab for LogsTab {
 	fn handle_input(&mut self, event: &ratatui::crossterm::event::Event) -> bool {
 		if let Event::Key(ev) = event {
-			let modifier = modifier_magnitude(ev) as u16;
+			let modifier = modifier_magnitude(ev) as usize;
 			match ev.code {
-				KeyCode::Up => self.scroll = self.scroll.saturating_sub(modifier),
-				KeyCode::Down => self.scroll = self.scroll.saturating_add(modifier),
+				KeyCode::Up => {
+					self.scroll = self.scroll.saturating_sub(modifier);
+					self.scroll_state = self.scroll_state.position(self.scroll);
+				},
+				KeyCode::Down => {
+					self.scroll = self.scroll.saturating_add(modifier);
+					self.scroll_state = self.scroll_state.position(self.scroll);
+				},
+				KeyCode::Esc => {
+					self.scroll = 0;
+					self.scroll_state.first();
+				},
 				_ => {},
 			}
 		}
@@ -26,13 +36,16 @@ impl super::Tab for LogsTab {
 
 impl super::Renderable for LogsTab {
 	fn render(&mut self, frame: &mut ratatui::Frame, area: ratatui::layout::Rect) {
-		frame.render_widget(LogsTabWidget(self.scroll), area);
+		frame.render_stateful_widget(LogsTabWidget(self.scroll), area, &mut self.scroll_state);
 	}
 }
 
 impl LogsTab {
 	pub fn new() -> Self {
-		Self { scroll: 0 }
+		Self {
+			scroll: 0,
+			scroll_state: ScrollbarState::default(),
+		}
 	}
 }
 
@@ -40,10 +53,12 @@ impl LogsTab {
 
 
 
-struct LogsTabWidget(u16);
+struct LogsTabWidget(usize);
 
-impl Widget for LogsTabWidget {
-	fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+impl StatefulWidget for LogsTabWidget {
+	type State = ScrollbarState;
+
+	fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer, state: &mut Self::State)
 	where
 		Self: Sized,
 	{
@@ -52,16 +67,28 @@ impl Widget for LogsTabWidget {
 			.title_alignment(ratatui::layout::HorizontalAlignment::Right)
 			.dark_gray();
 
-		Paragraph::new(
-			crate::logger::all().into_iter().rev().collect::<Vec<String>>()
-				.into_iter()
-				.map(Line::from)
-				.collect::<Vec<Line>>(),
-		)
-		.white()
-		.block(container)
-		.wrap(Wrap { trim: true })
-		.scroll((self.0, 0))
-		.render(area, buf);
+		let logs = crate::logger::all().into_iter().rev().collect::<Vec<String>>()
+			.into_iter()
+			.map(Line::from)
+			.collect::<Vec<Line>>();
+
+		*state = state.content_length(logs.len());
+
+		Paragraph::new(logs)
+			.white()
+			.wrap(Wrap { trim: false })
+			.block(container)
+			.scroll((self.0 as u16, 0))
+			.render(area, buf);
+
+		let scroll_area = area.inner(Margin {
+			// using an inner vertical margin of 1 unit makes the scrollbar inside the block
+			vertical: 1,
+			horizontal: 1,
+		});
+
+		Scrollbar::new(ratatui::widgets::ScrollbarOrientation::VerticalRight)
+			.style(Style::new().dark_gray())
+			.render(scroll_area, buf, state);
 	}
 }
